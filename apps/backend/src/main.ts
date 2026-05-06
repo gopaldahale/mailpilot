5c81ea58d7570a50e44a648b3a62ea9e3415f837
gopaldahale <gopaldahale00@gmail.com>
2026-05-06 10:54:01 +0530

    fix: removed broken submodule and re-added backend

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { Global, ValidationPipe } from '@nestjs/common';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // Security header 
+  app.use(helmet())
+
+  // Cookie parse 
+  app.use(cookieParser())
+
+  // Global Validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,   // removes extra fields
+    forbidNonWhitelisted: true,      // throws an error if extra fields are sent
+    transform: true,   // converts types (e.g., string "1" to number 1)
+  }))
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
