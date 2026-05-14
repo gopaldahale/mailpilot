33aff3c0b37b0e752fba48503218e1cd2285f71b
gdahale <gdahale@truperformance.us>
2026-05-14 16:03:34 +0530

    feat: setup Microsoft OAuth2 authentication

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { AuthService } from './auth.service.js';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Get('login')
   login(@Res() res: Response) {
     const url = this.authService.getMicrosoftAuthUrl();
 
     return res.redirect(url);
+  }
+  @Get('callback')
+  async callback(@Query('code') code: string) {
+    const tokens = await this.authService.exchangeCodeForToken(code);
+
+    const profile = await this.authService.getMicrosoftProfile(tokens.access_token,);
+
+    return { profile, tokens };
   }
 }
